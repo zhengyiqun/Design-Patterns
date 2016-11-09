d3dcf7340f9fb63a709c69bfba379fdb9e3a183c
zhengyiqun <zheng_yiqun@hotmail.com>
2016-11-09 16:34:50 +0800

    test

--- src/Design Patterns/Program.cs
+++ src/Design Patterns/Program.cs
@@ -6,7 +6,7 @@ namespace Design_Patterns
     {
         public static void Main(string[] args)
         {
-            //野鸭子
+            /*****野鸭子
             Duck test = new MallardDuck();
 
             test.performFly();
@@ -19,6 +19,19 @@ namespace Design_Patterns
             test2.performQuack();
 
             System.Console.ReadLine();
+            *****/
+
+            long lg = 5000000000000000000;            
+
+            System.Console.WriteLine( lg >> 0x10 );
+            System.Console.WriteLine( lg >> 0x18 );
+            System.Console.WriteLine( lg >> 0x20 );
+            System.Console.WriteLine( lg >> 40 );
+            System.Console.WriteLine( lg >> 0x30 );
+            System.Console.WriteLine( lg >> 0x38 );
+
+            uint num = (uint)(((lg | (lg << 8)) | (lg << 0x10)) | (lg << 0x18));
+            System.Console.WriteLine(num);
         }
     }
 }
